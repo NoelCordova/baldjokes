be8ce2766ba7201c8bcc99fc756016d6e8351478
Noel Espino Córdova <noelespinocordova@gmail.com>
2020-01-04 15:57:00 -0600

    Merge: rn-paper into develop (#1)
    * Add react-native-paper and react-native-vector-icons dependencies
    
    * Decrease the version of eslint community into 0.0.5
    
    * App structure
    
    * Update tests

--- __tests__/render-test.js
+++ __tests__/render-test.js
@@ -4,11 +4,16 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+
+import {App, HomeScreen} from './imports';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('Application renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('HomeScreen renders correctly', () => {
+  renderer.create(<HomeScreen />);
 });
